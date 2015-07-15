15d44d4764b0610f4d2cfdd7ff49b5e8ce3fcc8b
john lopez suarez <jlopez@utemvirtual.cl>
2015-07-15 12:55:11 -0300

    ADD: Base 2

--- base de datos/basebase.sql
+++ base de datos/basebase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 15-07-2015 a las 12:47:20
+-- Tiempo de generación: 15-07-2015 a las 15:54:47
 -- Versión del servidor: 5.5.20
 -- Versión de PHP: 5.3.10
 
@@ -23,6 +23,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `controlador`
+--
+
+CREATE TABLE IF NOT EXISTS `controlador` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `permiso_idpermiso` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_controlador_permiso1_idx` (`permiso_idpermiso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `permiso`
 --
 
@@ -34,13 +48,15 @@ CREATE TABLE IF NOT EXISTS `permiso` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `rol`
+-- Estructura de tabla para la tabla `privilegio`
 --
 
-CREATE TABLE IF NOT EXISTS `rol` (
+CREATE TABLE IF NOT EXISTS `privilegio` (
   `id` int(11) NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `controlador_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_privilegio_controlador1_idx` (`controlador_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -65,7 +81,6 @@ CREATE TABLE IF NOT EXISTS `usuario_has_rol` (
   `usuario_id` int(11) NOT NULL,
   `rol_id` int(11) NOT NULL,
   PRIMARY KEY (`usuario_id`,`rol_id`),
-  KEY `fk_usuario_has_rol_rol1_idx` (`rol_id`),
   KEY `fk_usuario_has_rol_usuario_idx` (`usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -74,11 +89,22 @@ CREATE TABLE IF NOT EXISTS `usuario_has_rol` (
 --
 
 --
+-- Filtros para la tabla `controlador`
+--
+ALTER TABLE `controlador`
+  ADD CONSTRAINT `fk_controlador_permiso1` FOREIGN KEY (`permiso_idpermiso`) REFERENCES `permiso` (`idpermiso`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `privilegio`
+--
+ALTER TABLE `privilegio`
+  ADD CONSTRAINT `fk_privilegio_controlador1` FOREIGN KEY (`controlador_id`) REFERENCES `controlador` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Filtros para la tabla `usuario_has_rol`
 --
 ALTER TABLE `usuario_has_rol`
-  ADD CONSTRAINT `fk_usuario_has_rol_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_usuario_has_rol_rol1` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_usuario_has_rol_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
